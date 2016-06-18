84857614f966d343d5c7f708a6e1ce4e2be2d395
Korirmitchelle <korirmitchelle@gmail.com>
2016-06-18 15:20:38 +0300

    personal initial commit

---
+++ javascript/mitch.js
@@ -0,0 +1,18 @@
+
+function myFunction() {
+    "use strict"; document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function (event) { "use strict";
+    
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+            
+    }
+};
