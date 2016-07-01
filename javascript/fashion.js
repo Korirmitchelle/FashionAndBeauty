7e04ecec0df0ccab792e5567d94a983aaa81f58f
Korirmitchelle <korirmitchelle@gmail.com>
2016-07-01 10:28:07 +0300

    js,edits

--- javascript/fashion.js
+++ javascript/fashion.js
@@ -3,7 +3,9 @@ var blue = document.getElementById("background");
 var orange = [
     "url('images/fashion2.jpg')",
     "url('images/makeup.jpg')",
-    "url('images/fashion3.jpg')"
+    "url('images/fashion3.jpg')",
+    "url('images/beauty.jpg')",
+    "url('images/tbdress.jpg')"
 
 ];
 var counter = 0;
